7aae62d0c92461b1a436628852e0337b15aca1a2
weestray <westray@bu.edu>
2024-11-20 20:36:27 -0500

    more

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,7 +24,12 @@ function closeInfo() {
 const monalisafactbox = document.getElementById('monalisafactbox');
 const leftButton = document.getElementById('leftButton');
 const rightButton = document.getElementById('rightButton');
-const monalisafacts = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape'];
+const monalisafacts = [
+`Before its theft in 1911, the Mona Lisa was well-regarded but not the global icon it is today. The heist made headlines worldwide and transformed the painting into a cultural treasure.`, 
+`Thief Vincenzo Peruggia, a former Louvre employee, disguised himself as a worker, hid in a storage closet overnight, and walked out with the painting under his apron.`, 
+`When the painting was first discovered missing, museum staff assumed it had been taken to be photographed or cleaned. It wasn’t until the next day that they realized it had been stolen.`, 
+`Pablo Picasso and his friend Guillaume Apollinaire were briefly questioned due to their known criticism of the Louvre.`, 
+`Peruggia was eventually caught when he tried to sell the painting two years later. As for his motive, he claimed to have stolen the painting as an act of patriotism, as he believed the Mona Lisa belonged in Italy, though it had been legally acquired by France centuries earlier.`];
 
 
 
